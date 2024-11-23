331553530285cfb4ec199edd10e8402bf7ffe03b
Sumile Turki <139162772+sumileturki@users.noreply.github.com>
2024-11-23 19:04:07 +0530

    Update Protected.jsx

--- src/pages/Protected.jsx
+++ src/pages/Protected.jsx
@@ -17,9 +17,10 @@ function Protected() {
     setAccount(account);
 
     
-    const timer = setTimeout(() => {
+   const timer = setTimeout(() => {
       window.open("https://weather-app-indol-xi.vercel.app/", "_blank"); 
     }, 1000);
+
 
 
     // Clear the timeout on component unmount
